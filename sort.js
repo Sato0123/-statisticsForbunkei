4a0b9608229f61a5d9ef56d53470b4a38e885d36
satoT <taichi.sato@links-system.co.jp>
2023-06-11 19:32:00 +0900

    j

--- sort.js
+++ sort.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var sortNumber = function (number) {
     number.sort(function (a, b) {
         if (a == b) {
@@ -10,7 +10,3 @@ var sortNumber = function (number) {
 
 var number = [19, 3, 81, 1, 24, 21];
 sortNumber(number);
-=======
-var number = [19, 3, 81, 1, 24, 21];
->>>>>>> 3130708f8a8ce5accac8e48c0120ee02af433613
-console.log(number);
